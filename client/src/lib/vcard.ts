77f3851542d43bf6fe214eeb81ae6c99f4695f7a
subbhas <subbha@myyahoo.com>
2025-07-03 23:06:30 -0700

    vcard changes final

--- client/src/lib/vcard.ts
+++ client/src/lib/vcard.ts
@@ -3,11 +3,11 @@ export function downloadVCard() {
 VERSION:3.0
 FN:Jayan Jayabal
 ORG:WealthX Financial Solutions
-TITLE:Entrepreneur
+TITLE:Entrepreneur & Wealth Advisor
 ADR:;;142 Esplanade East;North Vancouver;BC;V7L 4X9;Canada
 TEL:+1 (604) 704-7972
 EMAIL:jayan@wealthxfinancial.com
-URL:https://www.jayanjayabal.ca
+URL:https://jayanjayabal.ca
 END:VCARD`;
   
   const blob = new Blob([vCardData], { type: 'text/vcard' });
